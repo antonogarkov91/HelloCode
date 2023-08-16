6cd2be76d2d206067183ea60914e151bfa620d13
Anton_python <ogarkov.anton91@yandex.ru>
2023-08-16 12:27:12 +0300

    add Example 005 Condition IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "marry")
+{
+    Console.WriteLine("Ура, это же Marry");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
