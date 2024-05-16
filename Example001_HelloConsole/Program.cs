82c315f90ea3f6969040654829ac340d98c8c97f
Anton_python <ogarkov.anton91@yandex.ru>
2024-05-16 11:56:12 +0300

    add Example001

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+// // Console.WriteLine("Hello, World!");
+// // Console.WriteLine("Hello, World!");
+// // Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+System.Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
